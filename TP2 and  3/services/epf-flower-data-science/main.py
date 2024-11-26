f767647f21ef087b1c31e97ef226b00718ec06d0
tgimond <theo.gimond@epfedu.fr>
2024-11-26 11:02:02 +0100

    Ajout de la redirection de l'endpoint vers la documentation swagger automatique

--- TP2 and  3/services/epf-flower-data-science/main.py
+++ TP2 and  3/services/epf-flower-data-science/main.py
@@ -5,4 +5,4 @@ from src.app import get_application
 app = get_application()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", debug=True, reload=True, port=8080)
+    uvicorn.run("main:app",host="127.0.0.1", debug=True, reload=True, port=8080)
--- TP2 and  3/services/epf-flower-data-science/src/api/router.py
+++ TP2 and  3/services/epf-flower-data-science/src/api/router.py
@@ -1,8 +1,16 @@
 """API Router for Fast API."""
-from fastapi import APIRouter
+from fastapi import APIRouter, FastAPI
+from fastapi.responses import RedirectResponse
 
 from src.api.routes import hello
 
 router = APIRouter()
 
 router.include_router(hello.router, tags=["Hello"])
+
+app = FastAPI()
+app.include_router(router)
+
+@router.get("/", include_in_schema=False)
+async def root():
+    return RedirectResponse(url="/docs")
